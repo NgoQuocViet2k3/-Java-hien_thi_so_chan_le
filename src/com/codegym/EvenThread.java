4ee81bba6ff1e7ce473de5edd05465518cf372a1
ruukichi <ngovietlc93@gmail.com>
2022-01-27 17:06:14 +0700

    add

---
+++ src/com/codegym/EvenThread.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class EvenThread extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i+=2) {
+            System.out.println("Số chẵn = " + i);
+            try {
+                Thread.sleep(15);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
